c45457ab28900a8cc70434945e161900c8cab65b
John Bachir <j@jjb.cc>
2022-10-23 13:29:48 -0400

    comments about refinements in rails executor

--- lib/ruby-clock/runners.rb
+++ lib/ruby-clock/runners.rb
@@ -1,3 +1,9 @@
+# if we ever want to make these available at the top-level using refinements,
+# like we do with the DSL, it doesn't work in Rails because Rails prepends Kernel after
+# we use the refinment. Initial tests show that prepending ActiveSupport::ForkTracker::CoreExtPrivate
+# works, but maybe that's not reliable or has unknown side effects
+# https://stackoverflow.com/questions/74119178/
+
 module RubyClock::Runners
   def self.shell(string)
     RubyClock.instance.shell(string)
