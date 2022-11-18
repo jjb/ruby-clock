93c820df5deee859d5bc022be407154d302b8054
John Bachir <j@jjb.cc>
2022-11-18 01:54:02 -0500

    test cleanup

--- example-rails-7-app/app/models/example.rb
+++ example-rails-7-app/app/models/example.rb
@@ -1,17 +1,17 @@
 class Example < ApplicationRecord
   def self.check_for_global_method
     if defined?(schedule)
-      raise "💥 Oh no, the ruby-clock DSL is in the global environment! 💥"
+      "💥 Oh no, the ruby-clock DSL is in the global environment! 💥"
     else
-      "🦝"
+      "✅"
     end
   end
 
   def self.check_for_runner
     if defined?(shell) || defined?(rake)
-      raise "💥 Oh no, the runners got included in the global environment! 💥"
+      "💥 Oh no, the runners got included in the global environment! 💥"
     else
-      "🐅"
+      "✅"
     end
   end
 
