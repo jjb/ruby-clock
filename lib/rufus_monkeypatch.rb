0f01830514d38b5371447251aaab606d12f03dd8
John Bachir <j@jjb.cc>
2023-04-05 23:42:14 -0400

    remove `}` and `end` from autoname candidates

--- lib/rufus_monkeypatch.rb
+++ lib/rufus_monkeypatch.rb
@@ -3,10 +3,12 @@ class Rufus::Scheduler::Job
   def identifier
     @identifier ||= begin
       return name if name
+
       lines = handler.source.split("\n")
-      blank_removed = lines.reject(&:empty?)
-      comment_only_lines_removed = blank_removed.grep_v(/#.*/)
-      final_line_that_is_not_end = comment_only_lines_removed[-2].strip
+      whitespace_removed = lines.map(&:strip)
+      undesired_removed = whitespace_removed - ['', '}', 'end']
+      comment_only_lines_removed = undesired_removed.grep_v(/#.*/)
+      comment_only_lines_removed[-1] # final line
     rescue
       begin
         source_location.join('-')
