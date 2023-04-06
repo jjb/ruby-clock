c079321ccd3d59a2a253dad9e20a633ca6e0b3bb
John Bachir <j@jjb.cc>
2023-04-05 23:30:11 -0400

    refactor autoname code to be more readable

--- lib/rufus_monkeypatch.rb
+++ lib/rufus_monkeypatch.rb
@@ -2,7 +2,11 @@ require 'method_source'
 class Rufus::Scheduler::Job
   def identifier
     @identifier ||= begin
-      name || handler.source.split("\n").reject(&:empty?).grep_v(/#.*/)[-2].strip
+      return name if name
+      lines = handler.source.split("\n")
+      blank_removed = lines.reject(&:empty?)
+      comment_only_lines_removed = blank_removed.grep_v(/#.*/)
+      final_line_that_is_not_end = comment_only_lines_removed[-2].strip
     rescue
       begin
         source_location.join('-')
