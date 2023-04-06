994595eecc56d5b72826ff1c423419b93417f13d
John Bachir <j@jjb.cc>
2023-04-05 23:51:52 -0400

    don't generate array each time

--- lib/rufus_monkeypatch.rb
+++ lib/rufus_monkeypatch.rb
@@ -1,12 +1,13 @@
 require 'method_source'
 class Rufus::Scheduler::Job
+  UNDESIRED_ELEMENTS = ['', '}', 'end']
   def identifier
     @identifier ||= begin
       return name if name
 
       lines = handler.source.split("\n")
       whitespace_removed = lines.map(&:strip)
-      undesired_removed = whitespace_removed - ['', '}', 'end']
+      undesired_removed = whitespace_removed - UNDESIRED_ELEMENTS
       comment_only_lines_removed = undesired_removed.grep_v(/#.*/)
       comment_only_lines_removed[-1] # final line
     rescue
